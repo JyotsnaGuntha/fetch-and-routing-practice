9d10f048e7bd70555dd2e2f8e56e39a1285c2f3c
Jyotsna <147334973+JyotsnaGuntha@users.noreply.github.com>
2025-07-16 15:35:26 +0530

    Update index.js

--- src/components/BlogItemDetails/index.js
+++ src/components/BlogItemDetails/index.js
@@ -1,14 +1,13 @@
-// Write your JS code here
-
 import {Component} from 'react'
 import Loader from 'react-loader-spinner'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
 import './index.css'
 
 class BlogItemDetails extends Component {
   state = {blogData: {}, isLoading: true}
 
-  componentDidMount = () => {
+  componentDidMount() {
     this.getBlogItemData()
   }
 
@@ -19,38 +18,46 @@ class BlogItemDetails extends Component {
     const response = await fetch(`https://apis.ccbp.in/blogs/${id}`)
     const data = await response.json()
     const updatedData = {
-      id: data.id,
       title: data.title,
       imageUrl: data.image_url,
+      content: data.content,
       avatarUrl: data.avatar_url,
       author: data.author,
-      content: data.content,
-      topic: data.topic,
     }
+
     this.setState({blogData: updatedData, isLoading: false})
   }
 
-  render() {
-    const {blogData, isLoading} = this.state
-    const {title, imageUrl, avatarUrl, author, content} = blogData
+  renderBlogItemDetails = () => {
+    const {blogData} = this.state
+    const {title, imageUrl, content, avatarUrl, author} = blogData
+
     return (
-      <div className="blog-list-container">
+      <div className="blog-info">
+        <h1 className="blog-details-title">{title}</h1>
+
+        <div className="author-details">
+          <img className="author-pic" src={avatarUrl} alt={author} />
+          <p className="details-author-name">{author}</p>
+        </div>
+
+        <img className="blog-image" src={imageUrl} alt={title} />
+        <p className="blog-content">{content}</p>
+      </div>
+    )
+  }
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div className="blog-container">
         {isLoading ? (
           <div data-testid="loader">
             <Loader type="TailSpin" color="#00bfff" height={50} width={50} />
           </div>
         ) : (
-          <div className="blog-container">
-            <div className="blog-info">
-              <h2 className="blog-details-title">{title}</h2>
-              <div className="author-details">
-                <img className="author-pic" src={avatarUrl} alt={author} />
-                <p className="details-author-name">{author}</p>
-              </div>
-              <img className="blog-image" src={imageUrl} alt={title} />
-              <p className="blog-content">{content}</p>
-            </div>
-          </div>
+          this.renderBlogItemDetails()
         )}
       </div>
     )
